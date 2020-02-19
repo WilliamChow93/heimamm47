b4be31333c007ce88e422ca15bce6b43dd7bddea
william <499691225@qq.com>
2020-02-19 16:17:14 +0800

    创建login模块

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
+    <router-view></router-view>
     <el-button type="primary">主要按钮</el-button>
     <router-link to="#">点我啊</router-link>
-  <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 // 0. 导入 子组件
-
+import index from '../views/login/index.vue'
 // 1. 导入 vue-router
 import VueRouter from 'vue-router'
 // 2. 注册 vue-router
 Vue.use(VueRouter)
-
+//这里就是路由的配制项
+const routes = [
+    {
+        path: '/index',
+        component: index
+    },
+]
 // 3. 实例化
 const router = new VueRouter({
-    //这里就是路由的配制项
-    routes: [
-
-    ]
+    routes
 }) // 4. 需要在下面注入router
 export default router 
